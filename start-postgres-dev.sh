6fe983d757aef5faedc649f43fe78be8d4c793ef
grzzboot <per.andreas.engstrom@gmail.com>
2019-12-07 15:39:29 +0100

    Putting these aside

--- start-postgres-dev.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run --name dev-postgres -p 5432:5432 -d postgres:9.6-alpine
--- start-redis-dev.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run --name dev-redis -p 6379:6379 -d redis:5.0.7-alpine
