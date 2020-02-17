095542e59a1ccc247c08ee075e06c9c1f44a2697
grzzboot <per.andreas.engstrom@gmail.com>
2020-02-17 21:54:36 +0100

    Added HPA, vegeta and fix of expensive calculation

--- pingpong-service-simple/src/main/java/com/grzzboot/service/pingpong/resource/service/PingPongService.java
+++ pingpong-service-simple/src/main/java/com/grzzboot/service/pingpong/resource/service/PingPongService.java
@@ -5,7 +5,10 @@ import org.springframework.util.StringUtils;
 
 import com.grzzboot.service.pingpong.resource.service.domain.PingEntity;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class PingPongService {
 
 	private static final String MESSAGE_BASE = "Pong";
@@ -22,8 +25,16 @@ public class PingPongService {
 	}
 
 	private void doExpensiveCalculation() {
-		for (int i = 0; i < 50000000; i++) {
-			Math.log(i);
+		long start = System.currentTimeMillis();
+		for(int i = 0; i < 5000; i++) {
+			for(int j = 0 ; j < 10000; j++) {
+				Math.log(i*j);
+			}
+			long split = System.currentTimeMillis() - start;
+			if(split > 500) {
+				log.debug("Aborting expensive calculation after approx {} ms.", split);
+				return;
+			}
 		}
 	}
 
