fc6894296ad33ffc268e8b20504016b7b01b524a
grzzboot <per.andreas.engstrom@gmail.com>
2020-02-23 11:35:18 +0100

    Added vegeta attack files

---
+++ pingpong-service-simple/src/k8s/horizontal-pod-autoscaling/vegeta/attack-fast.sh
@@ -0,0 +1,4 @@
+echo "GET http://<YOUR-IP>:8080/ping?expensive=true" | vegeta attack -duration=10m -rate=16 | tee results.bin | vegeta report
+  vegeta report -type=json results.bin > metrics.json
+  cat results.bin | vegeta plot > plot.html
+  cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
